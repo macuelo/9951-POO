7f87226ec4744cf5cf8929cdb9298018cd8ff8b0
macuelo <bassflea123123@gmail.com>
2025-09-02 23:36:42 -0500

    Add files via upload

--- Usuario.java
+++ Usuario.java
@@ -1,8 +1,16 @@
 
 package modelo;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Usuario {
-    
+    //ATRIBUTOS
     private int id;
     private String nombre;
     private String correo;
@@ -19,4 +27,39 @@ public class Usuario {
     public String getNombre() { return nombre; }
     public String getCorreo() { return correo; }
     public String getPassword() { return password; }
+    
+    private Connection conn;
+    public UsuarioDAO(Connection conn) {
+        this.conn = conn;
+    }
+    //METODOS
+    
+        public void registrarUsuario(Usuario u) throws SQLException {
+        String sql = "INSERT INTO usuarios (nombre, correo, password) VALUES (?, ?, ?)";
+        PreparedStatement stmt = conn.prepareStatement(sql);
+        stmt.setString(1, u.getNombre());
+        stmt.setString(2, u.getCorreo());
+        stmt.setString(3, u.getPassword());
+        stmt.executeUpdate();
+    }
+
+    public List<Usuario> obtenerUsuarios() throws SQLException {
+        List<Usuario> lista = new ArrayList<>();
+        String sql = "SELECT * FROM usuarios";
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery(sql);
+
+        while (rs.next()) {
+            Usuario u = new Usuario(
+                rs.getInt("id_usuario"),
+                rs.getString("nombre"),
+                rs.getString("correo"),
+                rs.getString("password")
+            );
+            lista.add(u);
+        }
+        return lista;
+    }
+}
+
 }
